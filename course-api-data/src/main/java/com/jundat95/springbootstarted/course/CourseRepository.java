63e31948e127322c33ed90a97f1de4440d6322ec
Do Nhu Vy <donhuvy@hotmail.com>
2017-02-05 20:23:55 +0700

    Display default method by Spring Data JPA

--- course-api-data/src/main/java/com/jundat95/springbootstarted/course/CourseRepository.java
+++ course-api-data/src/main/java/com/jundat95/springbootstarted/course/CourseRepository.java
@@ -6,5 +6,15 @@ import java.util.List;
 
 public interface CourseRepository extends CrudRepository<Course, String> {
 
-    List<Course> findByTopicId(String topicId);
+    /**
+     * This is default method, create by Spring Data JPA automatically.
+     *
+     * @return
+     */
+    default List<Course> findByTopicId() {
+        return findByTopicId();
+    }
+
+    //List<Course> findByTopicId(String topicId);
+
 }
